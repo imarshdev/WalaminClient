b5e383097c7d325e952fc076263cb833eb6556a4
forevermarsh <marshmansur004@gmail.com>
2024-09-25 12:37:11 +0300

    gsjhsdjd

--- src/components/RideRequestForm.jsx
+++ src/components/RideRequestForm.jsx
@@ -3,8 +3,15 @@ import "../css/ride.css";
 import { TouchableOpacity } from "react-native-web";
 function RideRequestForm() {
   const [expanded, setExpanded] = useState(false);
+  const goUp = () => {
+    window.scrollTo({ top: window.innerHeight, behavior: "smooth" });
+    setExpanded(true);
+  };
   return (
-    <div className="container" style={{ padding: 0, margin: 0 }}>
+    <div
+      className="container"
+      style={{ padding: 0, margin: 0, height: "100vh" }}
+    >
       <div id="map"></div>
       <div
         className="bottomSheet"
@@ -13,11 +20,19 @@ function RideRequestForm() {
           transition: "height 0.5s",
         }}
       >
+        <div
+          style={{
+            width: "50px",
+            height: "3px",
+            backgroundColor: "darkgreen",
+            borderRadius: "10px",
+          }}
+        ></div>
         <div className="input-cover">
           <input
             type="text"
-            onFocus={() => setExpanded(true)}
-            onBlur={()=> setExpanded(false)}
+            onFocus={goUp}
+            onBlur={() => setExpanded(false)}
             style={{ width: "100%" }}
           />
         </div>
