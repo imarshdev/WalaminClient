0f3b8946770e395099364f0e353168ffcded14b6
forevermarsh <marshmansur004@gmail.com>
2024-09-25 09:33:42 +0300

    some day

--- src/components/RideRequestForm.jsx
+++ src/components/RideRequestForm.jsx
@@ -1,8 +1,27 @@
 import React, { useState, useRef } from "react";
+import "../css/ride.css";
+import { TouchableOpacity } from "react-native-web";
 function RideRequestForm() {
+  const [expanded, setExpanded] = useState(false);
   return (
-    <div className="container">
-      <h1>Ride Reguest</h1>
+    <div className="container" style={{ padding: 0, margin: 0 }}>
+      <div id="map"></div>
+      <div
+        className="bottomSheet"
+        style={{
+          height: expanded ? "90vh" : "30vh",
+          transition: "height 0.5s",
+        }}
+      >
+        <div className="input-cover">
+          <input
+            type="text"
+            onFocus={() => setExpanded(true)}
+            onBlur={()=> setExpanded(false)}
+            style={{ width: "100%" }}
+          />
+        </div>
+      </div>
     </div>
   );
 }
